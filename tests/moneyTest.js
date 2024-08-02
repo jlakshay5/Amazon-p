0f638b78cf2778083241c4cfcedcf46943ecf02c
jainlakshay21 <jainlakshay21@gmail.com>
2024-08-02 15:11:28 +0530

    Create automated tests for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,38 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+console.log('test suite: formatCurrency')
+
+console.log('convers cents to dollars');
+if(formatCurrency(2095)==='20.95'){
+  console.log('passed');
+}
+else{
+  console.log('failed');
+}
+
+console.log('works for 0');
+if(formatCurrency(0)==='0.00'){
+  console.log('passed');
+}
+else{
+  console.log('failed');
+}
+
+console.log('rounds up to tthe nearest cent');
+
+if(formatCurrency(2000.5)==='20.01'){
+  console.log('passed');
+}
+else{
+  console.log('failed');
+}
+
+
+
+if(formatCurrency(2000.4)==='20.00'){
+  console.log('passed');
+}
+else{
+  console.log('failed');
+}
+
