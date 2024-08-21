7adc2fbb3135c6b5488195c8396d1872bfda4858
jainlakshay21 <jainlakshay21@gmail.com>
2024-08-21 17:54:47 +0530

    learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,7 +4,54 @@ import { renderpaymentSummary } from './checkout/paymentSummary.js';
 //import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 import {loadProducts} from '../data/products.js';
-loadProducts(()=>{
+import { loadCart } from '../data/cart.js';
+
+Promise.all([
+  new Promise((resolve)=>{
+    loadProducts(()=>{
+      resolve('boogiewoogie');
+    });
+  
+  }),
+
+  new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve();
+    });
+  })
+
+]).then((val)=>{
+  console.log(val);
   renderOrderSummary();
-  renderpaymentSummary();
+  renderpaymentSummary(); 
 });
+
+/*
+new Promise((resolve)=>{
+  loadProducts(()=>{
+    resolve('boogiewoogie');
+  });
+
+}).then((value)=>{
+  console.log(value);
+  return new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve();
+    });
+  });
+
+}).then(()=>{
+renderOrderSummary();
+renderpaymentSummary(); 
+});
+*/
+
+// loadProducts(()=>{
+//   loadCart(()=>{
+//     renderOrderSummary();
+//     renderpaymentSummary(); 
+//   }); 
+// });
+
+
+
